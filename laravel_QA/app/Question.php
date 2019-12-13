1e66dbec9267d3c03a34ec61e577afc724b3b27f
hiep <hiepg198@gmail.com>
2019-12-14 01:04:36 +0700

    add view votes and answare on question

--- laravel_QA/app/Question.php
+++ laravel_QA/app/Question.php
@@ -29,4 +29,15 @@ class Question extends Model
     {
         return $this->created_at->diffForHumans();
     }
+
+    public function getStatusAttribute()
+    {
+        if($this->answers > 0) {
+            if($this->best_answers_id > 0) {
+                return "answered-accepted";
+            }
+            return "answered";
+        }
+        return "unanswred";
+    }
 }
